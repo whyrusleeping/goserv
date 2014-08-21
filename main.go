0147ab745f5c95aec8ce24c649ff094c96cfae39
Jeromy Johnson <jeromy.johnson@isilon.com>
2014-08-21 10:50:43 -0700

    add logging feature

--- main.go
+++ main.go
@@ -2,23 +2,41 @@ package main
 
 import (
 	"net/http"
-	"os"
+	"flag"
 	"fmt"
+	"log"
 )
+
+type GoservHandler struct {
+	serv http.Handler
+	logging bool
+}
+
+func (gh *GoservHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if gh.logging {
+		log.Printf("Serving: %s\n", req.URL.Path)
+	}
+	gh.serv.ServeHTTP(rw, req)
+}
 
 func main() {
 	port := 8080
 	dir := "."
+	dolog := flag.Bool("log", false, "enable logging")
+	flag.Parse()
 
-	if len(os.Args) > 1 {
-		dir = os.Args[1]
+	if len(flag.Args()) > 1 {
+		dir = flag.Arg(0)
 	}
 
 	serv := http.FileServer(http.Dir(dir))
+	gh := new(GoservHandler)
+	gh.logging = *dolog
+	gh.serv = serv
 
 	for port := 8080; port <= 65535; port++ {
 		fmt.Printf("Serving on port: %d\n", port)
-		http.ListenAndServe(fmt.Sprintf(":%d", port), serv)
+		http.ListenAndServe(fmt.Sprintf(":%d", port), gh)
 		fmt.Printf("Failed to serve on port: %d, retrying...\n", port)
 	}
 	if port > 65535 {
